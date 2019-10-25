43b5dce42075def22c8d52aa2b2d9f53c8aa1483
RJain7098 <rjain@stu.naperville203.org>
2019-10-25 11:25:48 -0500

    almmost done with the 156 program

---
+++ student e156/FirstLetterMap.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+/**
+   Read all words from a file and add them to a map
+   whose keys are the first letters of the words and
+   whose values are sets of words that start with
+   that same letter.
+   Then print out the word sets in alphabetical order.
+   Use the Java 8 merge() feature.
+*/
+public class FirstLetterMap
+{
+   public static void main(String[] args)
+   {
+      String filename = "test1.txt";
+
+      try (Scanner in = new Scanner(new File(filename)))
+      {
+
+         TreeMap<String, String> words = new TreeMap<>(); 
+         
+
+         while (in.hasNext())
+         {
+            String word = clean(in.next());
+            Character c = word.charAt(0);
+
+            // Update the map here
+            // Use the Java 8 merge method
+            if (words.containsKey(c.toString()))
+            {
+               String words = words.get(c.toString()); 
+               words += word; 
+               words.put(c.toString(), words); 
+            }
+
+         }
+
+         // Print the map here in this form
+         // a: [a, able, aardvark]
+         
+         
+      }
+      catch (FileNotFoundException e)
+      {
+         System.out.println("Cannot open: " + filename);
+      }
+   }
+
+   public static String clean(String s)
+   {
+      String r = "";
+      for (int i = 0; i < s.length(); i++)
+      {
+         char c = s.charAt(i);
+         if (Character.isLetter(c))
+         {
+            r = r + c;
+         }
+      }
+      return r.toLowerCase();
+   }
+
+}
